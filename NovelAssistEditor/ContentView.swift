c5e3bddad8cfa73c7bde8c4017673a03a881ba9e
鶴見駿 <architecturent1005@gmail.com>
2026-03-25 23:51:22 +0900

    first commit

---
+++ NovelAssistEditor/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NovelAssistEditor
+//
+//  Created by 鶴見駿 on 2026/03/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NovelAssistEditorTests/NovelAssistEditorTests.swift
@@ -0,0 +1,17 @@
+//
+//  NovelAssistEditorTests.swift
+//  NovelAssistEditorTests
+//
+//  Created by 鶴見駿 on 2026/03/25.
+//
+
+import Testing
+@testable import NovelAssistEditor
+
+struct NovelAssistEditorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
